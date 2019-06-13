c526c259757effad0791f420c22395d5bfba71a8
FernandoUFMG <51773254+FernandoUFMG@users.noreply.github.com>
2019-06-13 18:58:54 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,42 +5,37 @@
 #include "vectorCoordinates.cpp"
 #include "FileInput.cpp"
 #include "Ranking.cpp"
-#define tamanho 4
 
 using namespace std;
 
 int main(int argc,const char* argv[]){
+	
+	int tamanho = argc;
 	dataManagement f[tamanho];
 	dataStructuration g[tamanho];
 	string fileName[tamanho];
+	map<string,map<string,int>> tf;
 	
 	//Variaveis ranqueamento
 	map<int,map<string,int>> itf;
 	map<string,double> idf;
 	map<int,map<string,double>> W;
 	map<int,double> cos;
-	map <string,int > df;
 	
 	Ranking rank;
 	vector<string> frase;
 	
-	argv[0] = "teste.txt";
-	argv[1] = "teste2.txt";
-	argv[2] = "teste3.txt";
-	argv[3] = "teste4.txt";	
-	
 	for(int i = 0;i < tamanho;i++){
 		fileName[i] = argv[i];
-		//g[i].fillMap(fileName[i],f[i].getData(fileName[i]),df);
+		g[i].fillMap(fileName[i],f[i].getData(fileName[i]));
 	}
 	
 	for(int i = 0;i < tamanho;i++)
 		itf[i] = g[i].totalValue(f[i].getData(fileName[i]));
 		
 	idf = rank.calculateIDF(itf,tamanho); //preenche o map com os valores de IDF
-	W = rank.calculateW(idf,itf,tamanho); //Preenche o Map W, explicar melhor oq é o map w
+	W = rank.calculateW(idf,itf); //Preenche o Map W, explicar melhor oq Ã© o map w
 	frase = rank.readInput(); //Le a consulta
-	cos = rank.calculateCosine(frase,idf,W,tamanho); // calcula os valores do Ranking Cosseno de cada documento
-	rank.printRank(cos,tamanho); // Ordena os valores do cosseno do maior pro menor e printa na tela o ranking de documentos mais proximos da consulta
-	//rank.printRank(rank.calculateCosine(frase = rank.readInput(),idf = rank.calculateIDF(itf,tamanho),rank.calculateW(idf,itf)));
+	cos = rank.calculateCosine(frase,idf,W); // calcula os valores do Ranking Cosseno de cada documento
+	rank.printRank(cos); // Ordena os valores do cosseno do maior pro menor e printa na tela o ranking de documentos mais proximos da consulta
 }
